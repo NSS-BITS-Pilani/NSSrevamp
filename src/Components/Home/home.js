871c7f70fbdb6327f7575f78e168a7f5b60a799e
Nachiket <nachikotalwar@gmail.com>
2021-05-20 21:26:48 +0530

    added departments, to do - split this in smaller components

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -4,7 +4,6 @@ import classes from "./home.scss";
 const home = () => {
     return (
         <div>
-            
             <div className="initiatives">
                 <div className="section_heading">Our initiatives</div>
                 <div className="cards">
@@ -41,6 +40,82 @@ const home = () => {
                 </div>
               <div className="button_div">
                     <span className="button">View More</span>
+                </div>
+            </div>
+
+            <div className="departments">
+                <div className="section_heading">Our Departments</div>
+                <div className="cards">
+                    <div className="card">
+                        <div className = "card_heading">CLP</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">D3</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">Desco</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">EPD</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">HPA</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">Parishod</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">Events</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">School</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        
+                        <div className = "card_heading">Umang</div>
+                        <div className="card_info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.  
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
