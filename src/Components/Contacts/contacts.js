c29d3ad0c2163c3308d39518c90327fc8639f6b2
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2021-05-22 03:26:12 +0530

    Almost completed the desktop version of contact us component

--- src/Components/Contacts/contacts.js
+++ src/Components/Contacts/contacts.js
@@ -4,7 +4,7 @@ import classes from "./contacts.scss";
 const contacts = () => {
     return (
         <div className={`${classes.container} container`}>
-            <form action="#">
+            <form action="#" method="POST">
                 <h1>Contact Us</h1>
                 <label for="name">
                     <div className={`${classes.inputHead} inputHead`}>
@@ -18,13 +18,17 @@ const contacts = () => {
                     </div>
                 </label>
                 <input type="text" id="email" name="email" placeholder="abc@xyz.com" />
-                <label for="fname">
+                <label for="message">
                     <div className={`${classes.inputHead} inputHead`}>
                         Message
                     </div>
                 </label>
-                <input type="text" id="message" name="message" placeholder="Hey there, I wanted to say hi  and that "/>
+                <input type="text" id="message" name="message" placeholder="Hey there, I wanted to say hi and that" />
+                <input type="submit" value="Send message" />              
             </form>
+            <div className={`${classes.mapContainer} mapContainer`}>
+                <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=BITS%20Pilani+(NSS%20BITS%20Pilani)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            </div>
         </div>
     );
 }
