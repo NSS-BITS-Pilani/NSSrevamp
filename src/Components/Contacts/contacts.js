23c473b9dfb94b81e152d775dc8391a69d91e75e
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2021-05-21 16:15:29 +0530

    Added basic elements of contacts component

--- src/Components/Contacts/contacts.js
+++ src/Components/Contacts/contacts.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import "./contacts.scss";
+import classes from "./contacts.scss";
 
 const contacts = () => {
-    return <h1>Contacts</h1>
+    return (
+        <div className={`${classes.container} container`}>
+            <form action="#">
+                <h1>Contact Us</h1>
+                <label for="name">
+                    <div className={`${classes.inputHead} inputHead`}>
+                        Name
+                    </div>
+                </label>
+                <input type="text" id="name" name="name" placeholder="James Bond" />
+                <label for="email">
+                    <div className={`${classes.inputHead} inputHead`}>
+                        E-mail
+                    </div>
+                </label>
+                <input type="text" id="email" name="email" placeholder="abc@xyz.com" />
+                <label for="fname">
+                    <div className={`${classes.inputHead} inputHead`}>
+                        Message
+                    </div>
+                </label>
+                <input type="text" id="message" name="message" placeholder="Hey there, I wanted to say hi  and that "/>
+            </form>
+        </div>
+    );
 }
 
 export default contacts;
--- src/Components/Departments/departments.js
+++ src/Components/Departments/departments.js
@@ -41,8 +41,14 @@ const departments = () => {
                 <h2>WORKING:</h2>
                 <p>
                     NSS School facilitates the education of almost 100 students of classes 1-12. To bring about a deeper development of children, we push for a closer bond between the mentor and student by maintaining a 1:2 ratio of volunteers to students, by facilitating home visits of students by the volunteers and by Parent Volunteer meets.
+                </p>
+                <p>
                     To bring about an overall development of students, School also organises events Udaan and Sports Day to inculcate and promote cultural spirit and sportsmanship among the students. These events are wholly organised by the first year recruits of NSS giving them management expertise and chance to bond deeper with the students.
+                </p>
+                <p>
                     NSS School has also started programs for English improvement called English Learning Program and for competitive exams preparation called UNNATI. We also have a program for preparing students for the prestigious Jawahar Navodaya schools. These programs aim to give the children a better base for future development.
+                </p>
+                <p>
                     More importantly, School aims to generate a mentor-student relation between volunteers and students. Home visits, occasional workshops, festival celebration, birthday celebrations and different cultural and sports events are all key components of this endeavour. Over the years, many students and volunteers alike have left school with a deep place in their hearts for each other.
                 </p>
             </div>
