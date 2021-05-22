9ddff34c1dba624e24dd1ac4bdfac1faa6a1d71c
Nachiket <nachikotalwar@gmail.com>
2021-05-22 12:59:57 +0530

    home changes

--- src/Components/Home/DepCards/depcards.js
+++ src/Components/Home/DepCards/depcards.js
@@ -3,8 +3,13 @@ import classes from "./depcards.scss";
 
 const DepCard = (props) => {
     return (
-        <div className={`${classes.card} depcard`}>
-            <div className={`${classes.card_heading} card_heading`}>{props.depName}</div>
+        <div className={`${classes.depcard} depcard`}>
+            <div className={`${classes.card_heading} card_heading`}>
+                <div className={`${classes.card_color} card_color`}>
+                    <div>{props.depName}</div>
+                </div>
+                
+            </div>
             <div>
                 {props.depInfo}  
             </div>
