e46a9d38491c37025611b268c7652713c8e203f6
Nachiket <nachikotalwar@gmail.com>
2021-05-22 19:03:54 +0530

    minor change

--- src/Components/Aboutus/aboutus.js
+++ src/Components/Aboutus/aboutus.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import "./aboutus.scss";
+import classes from "./aboutus.scss";
 
 const aboutus = () => {
-    return <h1>About Us</h1>
+    return (
+        <div className={`${classes.container} container`}>
+            <div className={`${classes.head} head`}>About us</div>
+
+            
+            <div className={`${classes.aboutTop} aboutTop`}>
+                <div className={`${classes.aboutSection} aboutSection`}>
+                    <div className={`${classes.bullet} bullet`}>
+                        o
+                    </div>
+                    <div className={`${classes.title} title`}>
+                        Introduction
+                    </div>
+                    <div className={`${classes.aboutInfo} aboutInfo`}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porttitor dictum tincidunt lobortis nisi. Vitae turpis augue vitae tempor, tellus interdum aliquam imperdiet mi. Faucibus sit praesent phasellus tincidunt justo, quisque purus dignissim. Elementum imperdiet quis viverra malesuada mauris est. Malesuada dolor tristique nibh viverra penatibus suspendisse. Magna et elit sed amet, sit tortor tempus et. Velit vitae morbi proin at. Diam integer hac metus, at senectus ligula duis lectus. Blandit magna ut diam malesuada.
+                    </div>
+                </div>
+            </div>
+            
+            
+        </div>
+    );
 }
 
 export default aboutus;
