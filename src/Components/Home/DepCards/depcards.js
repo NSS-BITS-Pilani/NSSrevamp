29664206f4e792ad32f28ca519781fd592ecb1d2
Nachiket <nachikotalwar@gmail.com>
2021-05-22 15:51:34 +0530

    home updated

--- src/Components/Home/DepCards/depcards.js
+++ src/Components/Home/DepCards/depcards.js
@@ -4,13 +4,13 @@ import classes from "./depcards.scss";
 const DepCard = (props) => {
     return (
         <div className={`${classes.depcard} depcard`}>
-            <div className={`${classes.card_heading} card_heading`}>
+            <div style={{background:props.topColor}} className={`${classes.card_heading} card_heading`}>
                 <div className={`${classes.card_color} card_color`}>
                     <div>{props.depName}</div>
                 </div>
                 
             </div>
-            <div>
+            <div className={`${classes.depInfo} depInfo`}>
                 {props.depInfo}  
             </div>
         </div>
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -25,19 +25,69 @@ const Home = () => {
     const [ eventsExpanded, setEventsExpanded ] = useState(false);
 
     const departments = [
-        { depName: "CLP", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "D3", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "Desco", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "EPD", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "HPA", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "Parishod", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "Events", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "School", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." },
-        { depName: "Umang", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero." }
+        { depName: "CLP", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(255, 251, 165, 0.44)"},
+        { depName: "D3", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(165, 244, 255, 0.44)" },
+        { depName: "Desco", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(187, 107, 217, 0.23)" },
+        { depName: "EPD", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(170, 252, 204, 1)" },
+        { depName: "HPA", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(165, 244, 255, 0.44)" },
+        { depName: "Parishod", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(107, 217, 171, 0.23)" },
+        { depName: "Events", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(255, 190, 190, 0.43)" },
+        { depName: "School", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(165, 244, 255, 0.44)" },
+        { depName: "Umang", depInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit malesuada fames facilisis commodo, dui. Lorem ut suspendisse nec mauris consectetur ornare. Suspendisse condimentum massa, aliquet nec. Ut amet enim, in lectus scelerisque eget sodales est libero.", topColor:"rgba(187, 107, 217, 0.23)" }
     ];
     
     return (
         <div>
+            <div className={`${classes.mainInfo} mainInfo`}>
+                <div className={`${classes.homeFlexBox} homeFlexBox`}>
+                    <div className={`${classes.homeLeft} homeLeft`}>
+                        <div className={`${classes.section_heading} section_heading`}>
+                            NSS BITS PILANI<br></br>Not me but you
+                        </div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum erat egestas facilisis. Netus molestie nibh pellentesque magna lectus. Faucibus mattis massa sed scelerisque. Donec lacus, ut mauris ac in suspendisse amet lacinia arcu.
+                        <div className={`${classes.button_div} button_div`}>
+                            <span className={`${classes.button} button`}>View More</span>
+                        </div>
+                    </div>
+                    <div className={`${classes.homeRight} homeRight`}>
+                        <div><img src="/assets/home_top.png" alt="img" align="right"/></div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className={`${classes.homeAbout} homeAbout`}>
+                <div className={`${classes.section_heading} section_heading`}>Our initiatives</div>
+                <div className={`${classes.aboutInfo} aboutInfo`}>
+                    <div className={`${classes.aboutFlexBox} aboutFlexBox`}>
+                        <div className={`${classes.left} left`}>
+                            <div><img src="/assets/rectangle_24.png" alt="img" /></div>
+                        </div>
+
+                        <div className={`${classes.right} right`}>
+                            <div className={`${classes.section_heading} section_heading`}>
+                            <span style={{color: "rgba(39, 174, 96, 1)"}}>What are we</span>
+                            </div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum erat egestas facilisis. Netus molestie nibh pellentesque magna lectus. Faucibus mattis massa sed scelerisque. Donec lacus, ut mauris ac in suspendisse amet lacinia arcu.
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${classes.aboutInfo} aboutInfo`}>
+                    <div className={`${classes.aboutFlexBox} aboutFlexBox`}>
+                        <div className={`${classes.leftB} leftB`}>
+                            <div className={`${classes.section_heading} section_heading`}>
+                            <span style={{color: "rgba(45, 156, 219, 1)"}}>What we do</span>
+                            </div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Imperdiet et donec ut iaculis mauris amet fringilla neque eget. Habitant nec risus viverra at ut arcu, arcu venenatis. Lectus egestas diam at non nec mattis enim vivamus. Faucibus purus orci elit, id ornare pellentesque lectus aliquam. Ut leo consequat malesuada vel proin maecenas dictumst justo. Tristique nam iaculis amet augue justo volutpat varius imperdiet nunc.
+                        </div>
+                        <div className={`${classes.rightB} rightB`}>
+                            <div><img src="/assets/rectangle_24.png" align="right" alt="img" /></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
             <div className={`${classes.initiatives} initiatives`}>
                 <div className={`${classes.section_heading} section_heading`}>Our initiatives</div>
                 <div className={`${classes.cards} cards`}>
@@ -51,7 +101,10 @@ const Home = () => {
             <div className={`${classes.departments} departments`}>
                 <div className={`${classes.section_heading} section_heading`}>Our Departments</div>
                 <div className={`${classes.cards} cards`}>
-                    {departments.map((department) => <DepCard depName={department.depName} depInfo={department.depInfo}/>)}
+                    {departments.map((department) => <DepCard
+                        depName={department.depName}
+                        depInfo={department.depInfo}
+                        topColor={department.topColor}/>)}
                 </div>
             </div>
         </div>
