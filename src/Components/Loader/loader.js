b60da9379a7ce82480f3b8f1113b5ecd78583919
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2021-07-14 01:06:04 +0530

    Removed unused codes and files and downloaded the svg version of the logo

--- src/Components/Loader/loader.js
+++ src/Components/Loader/loader.js
@@ -1,32 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import classes from "./loader.scss";
 
-import * as location from './1055-world-locations.json'
-import * as sucess from './1127-success.json'
 import App from '../../App';
-
-const defaultOptions1 = {
-      loop: true,
-      autoplay: true, 
-      animationData: location.default,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
-      }
-    };
-
-    const defaultOptions2 = {
-      loop: true,
-      autoplay: true, 
-      animationData: sucess.default,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
-      }
-    };
 
 const Loader = () => {
 
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(undefined);
     const [completed, setCompleted] = useState(undefined);
 
     useEffect(() => {
@@ -35,23 +14,26 @@ const Loader = () => {
                 .then((response) => response.json())
                 .then((json) => {
                     setData(json);
-                    setLoading(true);
                 
                     setTimeout(() => {
                         setCompleted(true);
                     }, 1000);
             });
-        }, 3000);
+        }, 2000);
     }, []);
     return (
         <>
             {
                 !completed ? (
-                    <div className={`${classes.loaderWrapper} loaderWrapper`}>
-                          {/* ADD LOADER PAGE CONTENTS INSIDE THIS PARENT BLOCK ELEMENT */}
-                            <img src="./assets/nsslogo.png" />
+                    <>
+                        <div className={`${classes.loaderWrapper} loaderWrapper`}>
+                            {/* ADD LOADER PAGE CONTENTS INSIDE THIS PARENT BLOCK ELEMENT */}
+                            <div className="logo">
+                                <img src="./assets/nss.svg" alt="NSS Logo" />
+                            </div>
                             <h2>nss bits pilani</h2>
-                    </div>
+                        </div>
+                    </>
                 ) : (
                     <App />
                 )}
