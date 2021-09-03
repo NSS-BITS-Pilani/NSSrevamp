f8ae1e81589c0ea142e62d36e208a7f183966c57
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2021-09-03 19:55:24 +0530

    Home Page departments color box animation fixed a little

--- src/Components/Home/DepCards/depcards.js
+++ src/Components/Home/DepCards/depcards.js
@@ -9,16 +9,14 @@ const DepCard = (props) => {
    
     return (
         <HashLink to={`/departments/${props.depName}#depTop`}>
-        <div className={`${classes.depcard} depcard`}>
-            <div className={`${classes.card_heading} card_heading`}>
+            <div className={`${classes.depcard} depcard`}>
+                <div className={`${classes.card_heading} card_heading`}>
                     <div>{props.depName}</div>
-                   
-                <div className={`${classes.card_color} card_color`} style={{background:props.topColor}}></div>
-    
-            </div>
-            <div className={`${classes.depInfo} depInfo`}>
-                {props.depInfo}  
-            </div>
+                    <div className={`${classes.card_color} card_color`} style={{background:props.topColor}}></div>
+                </div>
+                <div className={`${classes.depInfo} depInfo`}>
+                    {props.depInfo}  
+                </div>
             </div>
         </HashLink>
     );
