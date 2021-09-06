3ff8359e3fec69781b833f8c76694fea3e557108
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2021-09-07 03:11:37 +0530

    Experiment Ring Reveal on the Logo

--- src/Components/Loader/loader.js
+++ src/Components/Loader/loader.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import classes from "./loader.scss";
 import { animated, useSpring } from 'react-spring'
+import Logo from "./Logo";
 
 const Loader = () => {
     // const [flip, set] = useState(false);
@@ -27,6 +28,16 @@ const Loader = () => {
         delay: 2000,
     });
 
+    const [flip, set] = useState(false);
+    const { x } = useSpring({
+        reset: true,
+        reverse: flip,
+        from: { x: 0 },
+        x: 1,
+        delay: 1000,
+        onRest: () => set(!flip),
+    });
+
     return (
         <React.Fragment>
             <div className={`${classes.loaderWrapper} loaderWrapper container-fluid`}>
@@ -34,7 +45,24 @@ const Loader = () => {
                     <img src="./assets/nss.svg" alt="NSS Logo" />
                 </animated.div> */}
                 <div className="logo row">
-                    <img src="./assets/nss.svg" alt="NSS Logo" />
+                    {/* <img src="./assets/nss.svg" alt="NSS Logo" /> */}
+                    <div className="logoComponent">
+                        <Logo />
+                    </div>
+                    <div className="ringReveal">
+                        <animated.svg
+                            style={{ margin: 20, width: 310, height: 310 }}
+                            viewBox="0 0 410 410"
+                            strokeWidth="7"
+                            fill="floralwhite"
+                            stroke="rgb(45, 55, 71)"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeDasharray={1256}
+                            strokeDashoffset={x.to(x => (1 - x) * 1256)}>
+                                <circle cx="205" cy="205" r="200" />
+                        </animated.svg>
+                    </div>
                 </div>
                 <div className="row">
                     <animated.h2 style={textProps} className="loaderText">nss bits pilani</animated.h2>
